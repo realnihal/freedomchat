0613e48f5698a80d23aaf19fe6ab179c251d296f
Nihal Puram <puramnihal@gmail.com>
2022-02-20 14:31:03 +0530

    bottom navigationbar in homepage

--- lib/main.dart
+++ lib/main.dart
@@ -17,15 +17,30 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-
 class _MyAppState extends State<MyApp> {
   FirebaseRepository _repository = FirebaseRepository();
 
   @override
   Widget build(BuildContext context) {
-  _repository.signOut();
+    _repository.signOut();
     return MaterialApp(
       title: "Skype Clone",
+      theme: ThemeData(
+        // Define the default brightness and colors.
+        brightness: Brightness.light,
+        primaryColor: Colors.purple[800],
+        backgroundColor: Colors.purple.shade50,
+        // Define the default font family.
+        fontFamily: 'Roboto',
+
+        // Define the default `TextTheme`. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
+      ),
       debugShowCheckedModeBanner: false,
       home: FutureBuilder(
         future: _repository.getCurrentUser(),
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,79 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  PageController pageController = PageController();
+  int _page = 0;
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Home Screen"),
+      backgroundColor: Colors.purple.shade50,
+      body: PageView(
+        children: [
+          Center(child: Text("Chat List Screen")),
+          Center(child: Text("Call losg")),
+          Center(child: Text("Contact Screen")),
+        ],
+        controller: pageController,
+        onPageChanged: onPageChanged,
+      ),
+      bottomNavigationBar: Container(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: CupertinoTabBar(
+            backgroundColor: Colors.purple.shade50,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.chat,
+                    color: (_page == 0)
+                        ? Colors.purple.shade300
+                        : Colors.purple.shade200),
+                title: Text(
+                    "Chat",
+                    style: TextStyle(
+                        fontSize: 10,
+                        color: (_page == 0) ? Colors.black : Colors.grey),
+                  ),
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.call,
+                      color: (_page == 1)
+                          ? Colors.purple.shade300
+                          : Colors.purple.shade200),
+                  title: Text(
+                    "Call",
+                    style: TextStyle(
+                        fontSize: 10,
+                        color: (_page == 1) ? Colors.black : Colors.grey),
+                  ),
+                  ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.contact_phone,
+                    color: (_page == 2)
+                        ? Colors.purple.shade300
+                        : Colors.purple.shade200),
+                title: Text(
+                    "Contact List",
+                    style: TextStyle(
+                        fontSize: 10,
+                        color: (_page == 2) ? Colors.black : Colors.grey),
+                  ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
