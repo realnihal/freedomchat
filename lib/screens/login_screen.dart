e84992619c9900bb1e9ead8285e9f8059d6a5845
Nihal Puram <puramnihal@gmail.com>
2022-02-20 11:46:17 +0530

    landing page

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, prefer_final_fields
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:freedomchat/resources/firebase_repository.dart';
@@ -15,24 +17,69 @@ class LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: loginButton(),
-    );
-  }
-
-  Widget loginButton() {
-    return FlatButton(
-      padding: EdgeInsets.all(35),
-      child: Text(
-        "LOGIN",
-        style: TextStyle(
-            fontSize: 35, fontWeight: FontWeight.w900, letterSpacing: 1.2),
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    "Welcome",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Freedom to communicate faster, better with safer",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                      fontSize: 15,
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height/3,
+                decoration: BoxDecoration(
+                  image: DecorationImage(image: AssetImage('assets/Illustration.png'))
+                ),
+              ),
+              Container(
+                child: MaterialButton(
+                  minWidth: double.infinity,
+                  height: 60,
+                  onPressed: () => performLogin(),
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: Colors.black
+                    ),
+                    borderRadius: BorderRadius.circular(50)
+                  ),
+                  child: Text("Login with Google",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),),
+              ),)
+            ],
+          ),
+        ),
       ),
-      onPressed: () => performLogin(),
     );
   }
 
+  
   void performLogin() async {
-
     UserCredential userCredential = await _repository.signIn();
     authenticateUser(userCredential);
   }
